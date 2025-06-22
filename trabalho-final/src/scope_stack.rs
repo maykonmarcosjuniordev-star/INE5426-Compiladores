acb330d54ab204e23633db6b1259896fa87b90cc
maykonmarcosjuniordev-star <maykon.marcos.junior.dev@gmail.com>
2025-06-22 17:54:27 -0300

    Adding scope stack

--- trabalho-final/src/scope_stack.rs
+++ trabalho-final/src/scope_stack.rs
@@ -2,25 +2,31 @@ use std::collections::HashMap;
 use std::error::Error;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum ScopeType {
+  Function,
+  Loop,
+  Any
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct SymbolEntry {
   appearances: Vec<usize>,
 }
 
-type Scope = HashMap<String, SymbolEntry>;
+type Scope = (ScopeType, HashMap<String, SymbolEntry>);
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ScopeStack {
   stack: Vec<Scope>,
 }
 
-
 impl ScopeStack {
   pub fn new() -> Self {
-    ScopeStack { stack: vec![HashMap::new()] }
+    ScopeStack { stack: vec![(ScopeType::Any, HashMap::new())] }
   }
 
-  pub fn push_scope(&mut self) {
-    self.stack.push(HashMap::new());
+  pub fn push_scope(&mut self, scope_type: ScopeType) {
+    self.stack.push((scope_type, HashMap::new()));
   }
 
   pub fn pop_scope(&mut self) -> Option<Scope> {
@@ -31,10 +37,11 @@ impl ScopeStack {
     // A pilha de escopo sempre deve ter pelo menos um escopo, então stack.last_mut() nunca deve retornar None.
     let Some(current_scope) = self.stack.last_mut() else { panic!("No current scope to insert symbol"); };
     // Se o escopo atual já contém o símbolo, retorna erro semântico de redefinição de símbolo.
-    if current_scope.contains_key(&name) {
+    let table = &mut current_scope.1;
+    if table.contains_key(&name) {
       return Err(format!("Erro semântico: Redefinição do símbolo '{}' ", name).into());
     }
-    current_scope.insert(name, entry);
+    table.insert(name, entry);
 
     Ok(())
   }
@@ -42,10 +49,15 @@ impl ScopeStack {
   pub fn get_symbol(&self, name: &str) -> Option<&SymbolEntry> {
     // Procura o símbolo nos escopos, começando do mais interno (topo da pilha).
     for scope in self.stack.iter().rev() {
-      if let Some(entry) = scope.get(name) {
+      if let Some(entry) = scope.1.get(name) {
         return Some(entry);
       }
     }
     None
+  }
+
+  pub fn contains(&self, scope_type: ScopeType) -> bool {
+    // Verifica se a pilha de escopo contém algum escopo do tipo especificado.
+    self.stack.iter().rev().any(|(st, _)| *st == scope_type)
   }
 }
